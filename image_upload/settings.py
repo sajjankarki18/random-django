75b4d98441626576db9bac40dea40a2f3d88c1de
sajan <sajjankarki10@gmail.com>
2024-05-11 12:25:27 +0545

    commit

--- image_upload/settings.py
+++ image_upload/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'image_upload.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "gallery_app",
+        'USER': 'postgres',
+        'PASSWORD':'sajan123',
+        'HOST': 'localhost'
     }
 }
 
